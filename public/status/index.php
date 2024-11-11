a6f11b8d05fa55bf06aabfa664162a6de693e533
Igor Gonçalves <igordossantos38@gmail.com>
2024-11-11 14:53:35 -0300

    feat: create a simple api code with a response

--- public/status/index.php
+++ public/status/index.php
@@ -1,2 +1,17 @@
 <?php
 // Arquivo de rotas
+
+require_once __DIR__ . "../../../api_core/config.php";
+require_once __DIR__ . "../../../api_core/Response.php";
+
+if (API_IS_ACTIVE) {
+  echo Response::json(200, "success", [
+    "version" => API_VERSION,
+    "status" => API_IS_ACTIVE
+  ]);
+} else {
+  echo Response::json(200, "success", [
+    "version" => API_VERSION,
+    "status" => "In maintence!"
+  ]);
+}
