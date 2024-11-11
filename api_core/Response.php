69bcf8350e60cc11fd079d6e5a2738f946d992f9
Igor Gonçalves <igordossantos38@gmail.com>
2024-11-11 15:08:51 -0300

    feat: add database and improve reponse with error status

--- api_core/Response.php
+++ api_core/Response.php
@@ -4,10 +4,25 @@ class Response {
   public static function json($status = 200, $message = "success", $data = null) {
     header("Content-Type: application/json");
 
+    // Chek if API is active
+    if (!API_IS_ACTIVE) {
+      return json_encode([
+        "status" => 400,
+        "message" => "api is not running",
+        "api_version" => API_VERSION,
+        "time_response" => time(),
+        "date" => date("d-M-Y H:i:s"),
+        "data" => null,
+      ], JSON_PRETTY_PRINT); // Formatação automática do JSO, isso é opcional
+    }
+
     return json_encode([
       "status" => $status,
       "message" => $message,
+      "api_version" => API_VERSION,
+      "time_response" => time(),
+      "date" => date("d-M-Y H:i:s"),
       "data" => $data,
-    ]);
+    ], JSON_PRETTY_PRINT);
   }
 }
---
+++ api_core/database.php
@@ -0,0 +1,84 @@
+<?php
+
+return [
+  [
+    'username' => 'jose.silva',
+    'email' => 'jose.silva@gmail.com'
+  ],
+  [
+    'username' => 'maria.oliveira',
+    'email' => 'maria.oliveira@yahoo.com'
+  ],
+  [
+    'username' => 'pedro.santos',
+    'email' => 'pedro.santos@outlook.com'
+  ],
+  [
+    'username' => 'ana.costa',
+    'email' => "",
+  ],
+  [
+    'username' => 'carlos.fernandes',
+    'email' => 'carlos.fernandes@gmail.com'
+  ],
+  [
+    'username' => 'juliana.souza',
+    'email' => 'juliana.souza@icloud.com'
+  ],
+  [
+    'username' => 'roberto.almeida',
+    'email' => "",
+  ],
+  [
+    'username' => 'fernanda.rocha',
+    'email' => 'fernanda.rocha@protonmail.com'
+  ],
+  [
+    'username' => 'marcos.ramos',
+    'email' => "",
+  ],
+  [
+    'username' => 'lucia.martins',
+    'email' => 'lucia.martins@hotmail.com'
+  ],
+  [
+    'username' => 'ricardo.lima',
+    'email' => 'ricardo.lima@yahoo.com'
+  ],
+  [
+    'username' => 'gabriela.dias',
+    'email' => 'gabriela.dias@gmail.com'
+  ],
+  [
+    'username' => 'andre.azevedo',
+    'email' => "",
+  ],
+  [
+    'username' => 'priscila.santos',
+    'email' => 'priscila.santos@outlook.com'
+  ],
+  [
+    'username' => 'sandro.moura',
+    'email' => 'sandro.moura@icloud.com'
+  ],
+  [
+    'username' => 'renata.cardoso',
+    'email' => "",
+  ],
+  [
+    'username' => 'felipe.souza',
+    'email' => 'felipe.souza@protonmail.com'
+  ],
+  [
+    'username' => 'rafaela.oliveira',
+    'email' => 'rafaela.oliveira@gmail.com'
+  ],
+  [
+    'username' => 'bruno.silveira',
+    'email' => 'bruno.silveira@yahoo.com'
+  ],
+  [
+    'username' => 'leticia.lopes',
+    'email' => "",
+  ],
+];
