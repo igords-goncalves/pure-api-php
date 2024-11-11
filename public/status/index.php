234db13143b6139fd66e691eda47381f5f79954c
Igor Gonçalves <igordossantos38@gmail.com>
2024-11-11 15:12:43 -0300

    refactor: remove unecessary api checking

--- public/status/index.php
+++ public/status/index.php
@@ -4,14 +4,7 @@
 require_once __DIR__ . "../../../api_core/config.php";
 require_once __DIR__ . "../../../api_core/Response.php";
 
-if (API_IS_ACTIVE) {
-  echo Response::json(200, "success", [
-    "version" => API_VERSION,
-    "status" => API_IS_ACTIVE
-  ]);
-} else {
-  echo Response::json(200, "success", [
-    "version" => API_VERSION,
-    "status" => "In maintence!"
-  ]);
-}
+echo Response::json(200, "success", [
+  "version" => API_VERSION,
+  "status" => API_IS_ACTIVE
+]);
